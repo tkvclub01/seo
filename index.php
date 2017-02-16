911465fecd5df1971b56d64fdea975459ef5faa9
tkvcl <tkvcl@DESKTOP-OH4D19R.net.fpt>
2017-02-16 20:10:54 +0700

    PL

--- index.php
+++ index.php
@@ -70,4 +70,18 @@ $results = $service->volumes->listVolumes('Henry David Thoreau', $optParams);
   <br />
 <?php endforeach ?>
 
+
+<div id="content"></div>
+    <script>
+      function hndlr(response) {
+      for (var i = 0; i < response.items.length; i++) {
+        var item = response.items[i];
+        // in production code, item.htmlTitle should have the HTML entities escaped.
+        document.getElementById("content").innerHTML += "<br>" + item.htmlTitle;
+      }
+    }
+    </script>
+    <script src="https://www.googleapis.com/customsearch/v1?key=<span class="apiparam">2jBhlbS0Wq8R8CPsrAZBeo0t</span>&amp;cx=017576662512468239146:omuauf_lfve&amp;q=cars&amp;callback=hndlr">
+    </script>
+
 <?php pageFooter(__FILE__); ?>
