75c1815a144d5b56f606d4e5ca5e4ebf5dbafd16
tkvcl <tkvcl@DESKTOP-OH4D19R.net.fpt>
2017-02-16 17:28:10 +0700

    INdex

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php 
+// include your composer dependencies
+require_once 'google-api-php-client/vendor/autoload.php';
+
+$client = new Google_Client();
+$client->setApplicationName("DATO-SEO");
+$client->setDeveloperKey("AIzaSyBf_MGPLbvI0KDALhNOhzT8Fkv-JKTnl90");
+
+$service = new Google_Service_Books($client);
+$optParams = array('filter' => 'free-ebooks');
+$results = $service->volumes->listVolumes('Henry David Thoreau', $optParams);
+
+foreach ($results as $item) {
+	echo $item['volumeInfo']['title'], "<br /> \n";
+}
+?>
