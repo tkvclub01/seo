4ff4c8d5c9bba7b351df8fceee13060d7d052379
tkvcl <tkvcl@DESKTOP-OH4D19R>
2017-02-17 14:40:22 +0700

    1

--- search.py
+++ search.py
@@ -2,4 +2,10 @@ import sys, json
 from google import google
 
 datas = sys.argv[1].replace("+", " ").split('@@')
-print datas[2]
+num_page = 1
+search_results = google.search(datas[2].strip(), num_page,"en",True,datas[0].strip(),datas[1].strip())
+index = len(search_results)
+result = '0@@'
+if index > 0:
+    result =   str(search_results[index-1].rank) + "@@" + str(search_results[index-1].link)
+print result    
