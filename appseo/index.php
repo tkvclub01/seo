cb3f746158afff25b271c129fdd066e1dc6e1e61
tkvcl <tkvcl@DESKTOP-OH4D19R>
2017-02-17 16:36:08 +0700

    1

--- appseo/index.php
+++ appseo/index.php
@@ -2,25 +2,6 @@
 	ob_start(); 
 	session_start();
 	
-	// set time for session timeout
-	$currentTime = time() + 25200;
-	$expired = 3600;
-	
-	// if session not set go to login page
-	if(!isset($_SESSION['user'])){
-		header("location:https://tools.clads.link/appseo");
-	}
-	
-	// if current time is more than session timeout back to login page
-	if($currentTime > $_SESSION['timeout']){
-		session_destroy();
-		header("location:https://tools.clads.link/appseo");
-	}
-	
-	// destroy previous session timeout and create new one
-	unset($_SESSION['timeout']);
-	$_SESSION['timeout'] = $currentTime + $expired;
-	
 	error_reporting(0);
 	$dir = "images/background/";
 	// Open a directory, and read its contents
