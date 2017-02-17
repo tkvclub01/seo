83fd87e17772fda14dff64465b5cb76c9a665b66
tkvcl <tkvcl@DESKTOP-OH4D19R>
2017-02-17 09:03:51 +0700

    Update search

--- py/search.py
+++ py/search.py
@@ -1,5 +1,6 @@
 from google import google
-num_page = 10
-search_results = google.search("dantri", num_page,"en",True,"my")
+num_page = 1
+search_results = google.search("dantri", num_page,"en",True,"my","fanny.com.vn")
 for search_result in search_results:
-    print search_result.link
+    print str(search_result.rank) + "--" + str(search_result.link)
+    
