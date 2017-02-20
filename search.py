265a74dc73d9f52b574571f758677f116957c79d
Thành Tân Ak <tkvclub01@hotmail.com>
2017-02-20 11:12:08 +0700

    update

--- search.py
+++ search.py
@@ -6,7 +6,7 @@ num_page = 1
 search_results = google.search(datas[2].strip(), num_page,"en",True,datas[0].strip(),datas[1].strip())
 index = len(search_results)
 result = '0@@ '
-if index >= 1:
+if search_results[index-1].rank >= 1:
     result =   str(search_results[index-1].rank) + "@@" + str(search_results[index-1].link)
 else:
     result = '0@@ '
